6455b2b388c7abada192fb13bc81650c50541ffe
Matheus <matheustrilhakoch@hotmail.com>
2023-12-27 21:33:52 -0300

    Completing exercises

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
 let express = require('express');
 let app = express();
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    // res.send("Hello Express");
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
 });
+app.get("/json", function(req, res) {
+    res.json({
+        "message": "Hello json"
+    })
+});
+
+// console.log("Hello World")
 
 
 
